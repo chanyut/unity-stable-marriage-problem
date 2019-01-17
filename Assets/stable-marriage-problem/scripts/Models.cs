18c749afe850fb4f6a901908c96c9e48a7ea0440
chanyut <chanyutx10@gmail.com>
2019-01-17 10:53:36 +0700

    answer here

--- Assets/stable-marriage-problem/scripts/Models.cs
+++ Assets/stable-marriage-problem/scripts/Models.cs
@@ -6,16 +6,17 @@ public class Person {
     public string name;
     public Person fiance;
     public List<Person> preferences;
+    public int currentPrefIdx;
 
     public Person(string name) {
         this.name = name;
         this.preferences = new List<Person>();
         this.fiance = null;
+        this.currentPrefIdx = 0;
     }
 
     public void DebugMyRelationship() {
-        Debug.AssertFormat(this.fiance != null, "fiance could not be null");
-        Debug.LogFormat("relationship: {0} - {1}", this.name, this.fiance.name);
+        Debug.LogFormat("relationship: {0} - {1}", this.name, this.fiance == null? "none": this.fiance.name);
     }
 
 }
--- Assets/stable-marriage-problem/scripts/StableMarriage.cs
+++ Assets/stable-marriage-problem/scripts/StableMarriage.cs
@@ -4,18 +4,71 @@ using UnityEngine;
 
 public class StableMarriage : MonoBehaviour
 {
-    void Start() {
-        var persons = TestCases.TestCase01();
-        this.RunAlgorithm(persons);
-        this.PrintAnswer(persons);
+    IEnumerator Start() {
+        var t = TestCases.TestCase01();
+        yield return this.RunAlgorithm(t.Item1, t.Item2);
+        this.PrintAnswer(t.Item1);
     }
     
     void PrintAnswer(Person[] persons) {
-
+        Debug.Log("answer...");
+        for (int i=0; i<persons.Length; i++) {
+            persons[i].DebugMyRelationship();
+        }
     }
 
-    void RunAlgorithm(Person[] persons) {
-        
+    IEnumerator RunAlgorithm(Person[] men, Person[] women) {
+        // reset state...
+        foreach (var m in men) {
+            m.currentPrefIdx = 0;
+        }
+
+        int round = 1;
+        while (true) {
+            bool stillHasASingleMan = false;
+            for (int i=0; i<men.Length; i++) {
+                if (men[i].fiance == null) {
+                    stillHasASingleMan = true;
+                    break;
+                }
+            }
+            if (!stillHasASingleMan) {
+                break;
+            }
+
+            for (int i=0; i<men.Length; i++) {
+                var m = men[i];
+                if (m.fiance != null) {
+                    continue;
+                }
+                for (; m.currentPrefIdx<m.preferences.Count; m.currentPrefIdx++) {
+                    var w = m.preferences[m.currentPrefIdx];
+                    if (w.fiance == null) {
+                        m.fiance = w;
+                        w.fiance = m;
+                        break;
+                    } else {
+                        var newPerfIdx = w.preferences.IndexOf(m);
+                        var oldPerfIdx = w.preferences.IndexOf(w.fiance);
+                        if (newPerfIdx < oldPerfIdx) {
+                            w.fiance.fiance = null;
+                            m.fiance = w;
+                            w.fiance = m;
+                            break;
+                        } else {
+                            // do nothing
+                        }
+                    }
+                }
+            }
+
+            // debug
+            Debug.LogFormat("round: {0}", round++);
+            foreach (var m in men) {
+                m.DebugMyRelationship();
+            }
+            yield return null;
+        }
     }
 
 }
