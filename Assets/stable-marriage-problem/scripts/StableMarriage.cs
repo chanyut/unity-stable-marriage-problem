22ce39171256e86da2060dff2483d07684ace135
chanyut <chanyutx10@gmail.com>
2019-01-17 11:02:13 +0700

    includes pseudo code

--- Assets/stable-marriage-problem/scripts/StableMarriage.cs
+++ Assets/stable-marriage-problem/scripts/StableMarriage.cs
@@ -21,4 +21,21 @@ public class StableMarriage : MonoBehaviour
         yield return null;
     }
 
+    /**
+    // reference - https://en.wikipedia.org/wiki/Stable_marriage_problem
+    function stableMatching {
+        Initialize all m ∈ M and w ∈ W to free
+        while ∃ free man m who still has a woman w to propose to {
+                w = first woman on m’s list to whom m has not yet proposed
+                if w is free
+                    (m, w) become engaged
+                else some pair (m', w) already exists
+                    if w prefers m to m'
+                    m' becomes free
+                    (m, w) become engaged 
+                    else
+                    (m', w) remain engaged
+            }  
+    }
+    */
 }
